ab7101d0e8a98e642805dd7985e8472a92f25052
Michael Aquilina <michaelaquilina@gmail.com>
2025-10-21 16:43:34 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+import time
+import functools
+from typing import Callable, Any
+from collections.abc import Iterable
+
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+
+
+def ttl_cache(func: Callable, ttl: int = 30) -> Callable:
+    cached_data: dict[str, tuple[float, Any]] = {}
+
+    @functools.wraps(func)
+    def cached_func(*args: Any, **kwargs: Any) -> Any:
+        key = "fixed"
+        now = time.time()
+
+        if entry := cached_data.get(key):
+            timestamp, value = entry
+            if now - timestamp > ttl:
+                value = func(*args, **kwargs)
+                cached_data[key] = (now, value)
+        else:
+            value = func(*args, **kwargs)
+            cached_data[key] = (now, value)
+
+        return value
+
+    return cached_func
+
+
+def search(value: str) -> Iterable[str]:
+    value = value.lower()
+
+    for item in get_all_passwords():
+        if value in item.lower():
+            yield item
+
+
+@ttl_cache
+def get_all_passwords() -> list[str]:
+    results = []
+    path = os.path.expanduser("~/.password-store")
+    for root, dirs, files in os.walk(path):
+        relative_path = root.removeprefix(path)
+        for file in files:
+            if file.endswith(".gpg"):
+                item = f"{relative_path}/{file}"
+                item = item.removeprefix("/")
+                item = item.removesuffix(".gpg")
+                results.append(item)
+
+    return results
+
+
+class PassExtension(Extension):
+    def __init__(self) -> None:
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(
+        self,
+        event: KeywordQueryEvent,
+        extension: Extension,
+    ) -> RenderResultListAction:
+        argument = event.get_argument() or ""
+
+        results: list[ExtensionResultItem] = []
+        for entry in search(argument):
+            results.append(ExtensionResultItem(
+                icon="images/icon.png",
+                name=entry,
+                description=entry,
+                # TODO: running with args does not seem to work
+                # generating a string this way is definitely not ideal
+                on_enter=RunScriptAction(f"pass -c {entry}"),
+            ))
+            if len(results) >= 5:
+                break
+
+        return RenderResultListAction(results)
+
+
+if __name__ == "__main__":
+    PassExtension().run()
